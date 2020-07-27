67da292aab336ae0ddf0acc22177ea23648de468
shaniaakhan21 <57680699+shaniaakhan21@users.noreply.github.com>
2020-07-27 17:11:48 +0530

    Update settings.py

--- proprogram/settings.py
+++ proprogram/settings.py
@@ -131,3 +131,8 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
