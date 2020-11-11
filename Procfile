047649f65225c314e4451ebe77f96d8c0909c278
shaniyakhan79 <shaniaakhan21@gmail.com>
2020-11-11 20:35:31 +0530

    changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn proprogram.wsgi
--- proprogram/settings.py
+++ proprogram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,6 +126,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'proprogram/static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Messages
 from django.contrib.messages import constants as messages
@@ -132,7 +134,9 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
+
+django_heroku.settings(locals())
